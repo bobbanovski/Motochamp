a79b339c360edf0dc9425d1f0b27271bd3749ab8
DevOps\Developer <bobbanovski@gmail.com>
2017-04-12 21:56:12 +1000

    Generate motorbikeform component

---
+++ ClientApp/app/components/motorbike-form/motorbike-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-motorbike-form',
+  templateUrl: './motorbike-form.component.html',
+  styleUrls: ['./motorbike-form.component.css']
+})
+export class MotorbikeFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
